a410a3b34ad6be8457335e65ca764a397d7cd0ee
Manchas-2 <167267501+Manchas-2@users.noreply.github.com>
2024-06-18 22:58:02 -0500

    Update manual.1.ino

--- Project.1/Automatico/manual.1.ino
+++ Project.1/Automatico/manual.1.ino
@@ -1,25 +1,27 @@
+//DECLARAR LIBRERIAS 
 #include "BluetoothSerial.h"
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-const int oneWireBus = 4;
-const int rest = 14;  
-const int dis = 27;
-BluetoothSerial SerialBT;
+const int oneWireBus = 4; //PIN sensor
+const int rest = 14; // PIN resistencia de calefacción
+const int dis = 27;  // PIN disipador 
+BluetoothSerial SerialBT; 
+
+//Decarar funciones del sensor 
 OneWire oneWire(oneWireBus);
 DallasTemperature sensors(&oneWire);
-float mx = 32.1;
-char DATO = '0';
-void setup() {
-    // Ajustar la velocidad para el monitor serie
-  Serial.begin(115200);
-  sensors.begin();
-  SerialBT.begin("ESP32test"); //Bluetooth device name
 
-  // Configurar pines como salidas
+float mx = 33.8; // temperatra tope de calefacción
+
+void setup() {
+ Serial.begin(115200); // Ajustar la velocidad para el monitor serie
+ sensors.begin();
+ SerialBT.begin("ESP32test"); //Bluetooth device name
+
+ // Configurar pines como salidas
   pinMode(rest, OUTPUT);
   pinMode(dis, OUTPUT);
-  
 }
 
 void loop() {
@@ -27,54 +29,61 @@ void loop() {
   Serial.println("Lectura");
   sensors.requestTemperatures();
 
-  // Lectura en grados Celsius
+ // Lectura en grados Celsius
   float temp = sensors.getTempCByIndex(0);
-  
-  if (temp == DEVICE_DISCONNECTED_C) {
-    Serial.println("Error: No se pudo leer el sensor de temperatura!");
-    delay(2000);
-    return; // Salir del loop y esperar para intentar de nuevo
+
+ //Corroborar que la lectura del sensor  
+  if (temp == DEVICE_DISCONNECTED_C) {     
+   Serial.println("Error: No se pudo leer el sensor de temperatura!");
+   delay(2000);
+   return; // Salir del loop y esperar para intentar de nuevo
+ }
+
+ //impreción de datos 
+  Serial.print("Temperatura sensor: ");
+  Serial.print(temp);
+  Serial.println("°C");
+
+ //Ciclo de calefacción 
+  while (temp < mx) {
+   // Impresión en Monitor serial
+    Serial.print("Temperatura= ");
+    Serial.print(temp);
+    Serial.println(" C°");
+    Serial.println("Calentando...");
+   // Impresión en Bluetooth
+    SerialBT.print("Temperatura= ");
+    SerialBT.print(temp);
+    SerialBT.println(" C°");
+    SerialBT.println("Calentando...");
+   // Funcionamiento de la calefacción
+    digitalWrite(rest, HIGH);
+    delay(16000);
+    digitalWrite(rest, LOW);
+   //nueva lesctura de temperatura
+    sensors.requestTemperatures();
+    temp = sensors.getTempCByIndex(0);
+    delay(4000);//Tiempo para estabilizar la temperatura 
+  } 
+ //Ciclo de disipación 
+  while (temp >= mx) {
+   // Impresión en Monitor serial
+    Serial.print("Temperatura= ");
+    Serial.print(temp);
+    Serial.println(" C°");
+    Serial.println("Listo.");
+   // Impresión en Bluetooth
+    SerialBT.print("Temperatura= ");
+    SerialBT.print(temp);
+    SerialBT.println(" C°");
+    SerialBT.println("Listo.");
+   //Funcionamiento del dispador
+    digitalWrite(dis, HIGH);
+    delay(5000);
+    digitalWrite(dis, LOW);
+   //Toma de temperatura
+    sensors.requestTemperatures(); 
+    temp = sensors.getTempCByIndex(0);
+        delay(1500);
   }
-   Serial.print("Temperatura sensor: ");
-      Serial.print(temp);
-      Serial.println("°C");
-
-      while (temp < mx) {
-        // Impresión en Monitor serial
-        Serial.print("Temperatura= ");
-        Serial.print(temp);
-        Serial.println(" C°");
-        Serial.println("Calentando...");
-        // Impresión en Bluetooth
-        SerialBT.print("Temperatura= ");
-        SerialBT.print(temp);
-        SerialBT.println(" C°");
-        SerialBT.println("Calentando...");
-        digitalWrite(rest, HIGH);
-        delay(1500);
-        digitalWrite(rest, LOW);
-        sensors.requestTemperatures(); // Se toma nuevamente la temperatura
-        temp = sensors.getTempCByIndex(0);
-        delay(2500);
-      }
-
-      while (temp >= mx) {
-        // Impresión en Monitor serial
-        Serial.print("Temperatura= ");
-        Serial.print(temp);
-        Serial.println(" C°");
-        Serial.println("Listo.");
-        // Impresión en Bluetooth
-        SerialBT.print("Temperatura= ");
-        SerialBT.print(temp);
-        SerialBT.println(" C°");
-        SerialBT.println("Listo.");
-        digitalWrite(dis, HIGH);
-        delay(1500);
-        digitalWrite(dis, LOW);
-        sensors.requestTemperatures(); // Se toma nuevamente la temperatura
-        temp = sensors.getTempCByIndex(0);
-        delay(2500);
-      }
-
 }
